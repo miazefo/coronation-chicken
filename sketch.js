762adce39476b9f24afa9d7c9f007d663c54f5ba
hannahweedy <117755328+hannahweedy@users.noreply.github.com>
2024-12-10 16:36:25 +0000

    star added

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 500);
 }
 
 function draw() {
   //change the colour of the background using a hex code
-  background(220);
+  background('#00ff00');
   //draw a tree shape in the centre of the screen
+textSize(30)
+text('⭐️',286,70);
+  
+noStroke();
+    beginShape();
+    vertex(300, 70);
+    vertex(260, 150);
+    vertex(280, 150);
+    vertex(220, 230);
+    vertex(260, 230);
+    vertex(190, 310);
+    vertex(280, 310);
+    vertex(280, 350);
+    vertex(320, 350);
+    vertex(320, 310);
+    vertex(410, 310);
+    vertex(340, 230);
+    vertex(380, 230);
+    vertex(320, 150);
+    vertex(340, 150);
+    endShape();
+
+// add presents
+
 }
