16948e2395e203c018ed612c27a486ef72173fa3
miazefo <m.zeferino-birchall0220221@arts.ac.uk>
2024-12-10 16:14:34 +0000

    corry-chick1

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //change the colour of the background using a hex code
+  background(220);
+  //draw a tree shape in the centre of the screen
+}
